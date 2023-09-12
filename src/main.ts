83d31342e3744fc0be442365868722c722865a92
Jose Vasquez <josevasquez8365@gmail.com>
2023-09-11 21:46:49 -0400

    last fix

--- src/main.ts
+++ src/main.ts
@@ -9,42 +9,76 @@ let dragElement: HTMLElement;
 let lastTarget: HTMLElement;
 
 imagen.addEventListener("dragstart", (e) => {
+ 
   const target = e.target as HTMLElement;
   dragElement = target;
-  const parent = target.parentNode as HTMLElement;
-  if (parent) lastTarget = parent;
+  const parent = target.parentNode as HTMLElement
+  if(parent) lastTarget = parent
+  
+
+  
 });
 
-imagen.addEventListener("dragend", () => {
-  cuadros.forEach((cuadro) => {
-    cuadro.classList.remove("empty");
-    cuadro.classList.remove("hover");
-  });
-  imagen.style.left = "0";
-});
+imagen.addEventListener("dragend", ()=> {
+  setTimeout(()=> {
+    imagen.classList.add("imagen");
+    imagen.classList.remove("hidden");
+  },50)
+  
+})
+
+imagen.addEventListener("drag", ()=> {
+  imagen.classList.remove("imagen")
+  imagen.classList.add("hidden")
+  
+})
+imagen.addEventListener("dragover",()=> {
+  imagen.classList.add("imagen")
+  imagen.classList.remove("hidden")
+})
 
 cuadros.forEach((cuadro) => {
+
+  
   cuadro.addEventListener("dragover", (e) => {
     e.preventDefault();
+    const target = e.target as HTMLElement
+    if(target) lastTarget = target
+
+    
   });
   cuadro.addEventListener("dragenter", () => {
-    imagen.style.left = "-3000%";
+    
+   
+    
+  
     cuadros.forEach((cuadro) => cuadro.classList.remove("hover"));
     cuadro.classList.add("hover");
-    cuadro.classList.remove("empty");
+  
   });
   cuadro.addEventListener("dragleave", () => {
-    cuadro.classList.add("empty");
+    
     cuadro.classList.remove("hover");
   });
   cuadro.addEventListener("drop", (e) => {
     e.preventDefault();
-    
+  
     const target = e.target as HTMLElement;
-    if (target.className.includes("cuadro")) {
-      target.appendChild(dragElement);
+  
+    if (dragElement !== target && !target.contains(dragElement)) {
+      if (target.className.includes("cuadro")) {
+        target.appendChild(dragElement);
+      } else {
+        lastTarget.appendChild(dragElement);
+        
+      }
+  
       target.classList.remove("hover");
+      imagen.classList.add("imagen");
+      imagen.classList.remove("hidden");
     }
-    imagen.style.left = "0";
   });
 });
+
+
+
